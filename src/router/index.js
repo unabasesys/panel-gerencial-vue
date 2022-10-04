0c5b0ce9aa043da68b6bebe56385aa9c660a61d7
rober97 <roberastorgab@gmail.com>
2022-10-04 09:40:54 -0300

    PRIMER

--- src/router/index.js
+++ src/router/index.js
@@ -5,61 +5,47 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/',
-        name: 'Home',
-        component: Home
+    path: '/*',
+    name: 'Home',
+    component: Home
+},
+{
+    path: '/dashboard',
+    name: 'Dashboard',
+    props: true,
+    meta: {
+        reload: true,
     },
-    {
-        path: '/panel',
-        name: 'Panel',
-        props: true,
-        meta: {
-            reload: true,
-        },
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/Panel.vue')
-    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import( /* webpackChunkName: "about" */ '../views/Dashboard.vue')
+},
 
-    {
-        path: '/dashboard',
-        name: 'Dashboard',
-        props: true,
-        meta: {
-            reload: true,
-        },
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/Dashboard.vue')
+{
+    path: '/panel',
+    name: 'Panel',
+    props: true,
+    meta: {
+        reload: true,
     },
-
-    {
-        path: '/panel',
-        name: 'Panel',
-        props: true,
-        meta: {
-            reload: true,
-        },
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/Panel.vue')
-    },
-    {
-        path: '/configuracion',
-        name: 'Configuracion',
-        props: true,
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/Configuracion.vue')
-    }
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import( /* webpackChunkName: "about" */ '../views/Panel.vue')
+},
+{
+    path: '/configuracion',
+    name: 'Configuracion',
+    props: true,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import( /* webpackChunkName: "about" */ '../views/Configuracion.vue')
+}
 ]
 
 const router = new VueRouter({
