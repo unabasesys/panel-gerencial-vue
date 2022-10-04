273fb41e5f7173f68ef3d758d502b072d23da37f
rober97 <roberastorgab@gmail.com>
2022-10-04 09:50:00 -0300

    PRIMER6668

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const routes = [{
     meta: {
         reload: true,
     },
-    // route level code-splitting
+    // route level code-splittingi
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
