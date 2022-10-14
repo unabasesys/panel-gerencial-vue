34b3fec5629df6fc45376d09c54ba9cbbfd4be61
rober97 <roberastorgab@gmail.com>
2022-10-14 12:10:56 -0300

    GUARDADO DE FILTRO VISIBLES EN BD 2

--- src/components/graphics/rentabilidad.vue
+++ src/components/graphics/rentabilidad.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-card class="pa-4 rounded-box-div" max-width="900" max-height="400">
+    <v-card class="pa-4 rounded-box-div" max-width="900" max-height="500">
       <span class="nunito-bold-bright-gray-18px">Rentabilidad</span>
 
       <selectGraphic />
@@ -14,7 +14,7 @@
         :css-classes="cssClasses"
         :styles="styles"
         :width="300"
-        :height="200"
+        :height="220"
       />
     </v-card>
   </div>
