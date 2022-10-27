9ad6cebaf9a88cb0ea29bf95eb1a3436cfbb9188
rober97 <roberastorgab@gmail.com>
2022-10-27 12:59:38 -0300

    Mejora al llamado de mis conexiones

--- src/components/graphics/tareas.vue
+++ src/components/graphics/tareas.vue
@@ -1,72 +1,72 @@
 <template>
   <div>
-      <span class="nunito-bold-bright-gray-18px">Mis tareas</span>
-      <v-row class="mt-1">
-          <v-col
-            cols="4"
-            v-for="item in indicadores"
-            :key="item.name"
-            style="margin-right: 60px"
-          >
-            <v-card
-              class="rounded-box"
-              width="170"
-              height="120"
-              color="#F0F5FF"
-              outlined>
-              <div class="row mt-1 ml-2">
-                <div cols="6" style="min-width: 145px">
-                  <span class="nunito-semi-bold-santas-gray-12px">{{
-                    item.name
-                  }}</span>
-                </div>
-                <div cols="6">
-                  <v-icon size="12" color="#98A2B3">ub-tres_puntos</v-icon>
-                </div>
+    <span class="nunito-bold-bright-gray-18px">Mis tareas</span>
+    <v-row class="mt-1">
+      <v-col
+        cols="4"
+        v-for="item in indicadores"
+        :key="item.name"
+        style="margin-right: 60px"
+      >
+        <v-card
+          class="rounded-box"
+          width="170"
+          height="120"
+          color="#F0F5FF"
+          outlined
+        >
+          <div class="row mt-1 ml-2">
+            <div cols="6" style="min-width: 140px">
+              <span class="nunito-semi-bold-santas-gray-12px">{{
+                item.name
+              }}</span>
+              <v-btn icon @click="toRendiciones()">
+                <v-icon size="12" color="#98A2B3"
+                  >fa-solid fa-square-arrow-up-right</v-icon
+                >
+              </v-btn>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div cols="6">
+              <div class="circle-responsive ml-4">
+                <v-avatar color="#FFFFFF" size="50">
+                  <v-icon size="40" color="#6871EC">{{ item.icon }}</v-icon>
+                </v-avatar>
               </div>
+            </div>
 
-              <div class="row mt-4">
-                <div cols="6">
-                  <div class="circle-responsive ml-4">
-                    <v-avatar color="#FFFFFF" size="50">
-                      <v-icon size="40" color="#6871EC">{{ item.icon }}</v-icon>
-                    </v-avatar>
-                  </div>
-                </div>
+            <div cols="6" class="mt-2">
+              <span class="nunito-normal-16px">{{ item.nValue }}</span>
+            </div>
+          </div>
 
-                <div cols="6" class="mt-2">
-                  <span class="nunito-normal-16px">{{ item.nValue }}</span>
-                </div>
-              </div>
+          <div class="row" style="margin-top: -10px">
+            <div cols="6" style="min-width: 115px">
+              <span class="nunito-semi-bold-santas-gray-10px ml-6"
+                >Information text</span
+              >
+            </div>
 
-              <div class="row">
-                <div cols="6" style="min-width: 115px">
-                  <span class="nunito-semi-bold-santas-gray-10px ml-6"
-                    >Information text</span
-                  >
-                </div>
-
-                <div cols="6" class="">
-                  <span class="nunito-semi-bold-emerald-14px"
-                    ><v-icon class="mr-1" size="15">ub-percent</v-icon>+{{
-                      item.percent
-                    }}%</span
-                  >
-                </div>
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
-
-       
+            <div cols="6" class="">
+              <span class="nunito-semi-bold-emerald-14px"
+                ><v-icon class="mr-1" size="15">ub-percent</v-icon>+{{
+                  item.percent
+                }}%</span
+              >
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-
 import axios from "axios";
 export default {
-    data() {
+  data() {
     return {
       indicadores: [
         {
@@ -77,18 +77,26 @@ export default {
         },
         {
           name: "Rendiciones vencidas",
-          nValue: this.randomNum(),
+          nValue: 0,
           icon: "ub-papel",
           percent: 15,
         },
-        
       ],
     };
   },
 
   methods: {
+    toRendiciones() {
+      let url = this.$route.query.url;
+      if (url != undefined) {
+        window.open(
+          `https://${url}/4DACTION/info_rf_vencidas?fromDashboard=true`,
+          "_blank" // <- This is what makes it open in a new window.
+        );
+      }
+    },
     randomNum() {
-      let res = parseInt(Math.random() * (20));
+      let res = parseInt(Math.random() * 20);
       return this.formatNumber(res);
     },
     formatNumber(num) {
@@ -101,7 +109,7 @@ export default {
 
     async fethData() {
       let url = this.$route.query.url;
-      let username = this.$route.query.user
+      let username = this.$route.query.user;
 
       let date = new Date();
       let currentYear = date.getFullYear();
@@ -123,17 +131,17 @@ export default {
       };
 
       axios(config).then((respuestas) => {
-        this.indicadores[0].nValue = respuestas.data[0].total_doc_por_aprobar
+        this.indicadores[0].nValue = respuestas.data[0].total_doc_por_aprobar;
+        this.indicadores[1].nValue = respuestas.data[0].total_rendiciones_vencidas;
       });
     },
   },
 
   async mounted() {
-    this.fethData()
+    this.fethData();
   },
-}
+};
 </script>
 
 <style>
-
 </style>
