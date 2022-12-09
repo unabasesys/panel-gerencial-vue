4d3d65c6543894b3deb9835d8c13d56db26da6d1
rober97 <roberastorgab@gmail.com>
2022-12-09 16:52:29 -0300

    TEST 2

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,6 @@ const routes = [{
         reload: true,
     },
     // route level code-splittingi
-    // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
         import( /* webpackChunkName: "about" */ '../views/Dashboard.vue')
