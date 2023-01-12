429c55196cc354580635831ade226b3974dce17b
rober97 <roberastorgab@gmail.com>
2023-01-12 10:17:59 -0300

    NO SE

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.html',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  
   transpileDependencies: [
     'vuetify'
   ],
