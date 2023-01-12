e4dcc9545011e54ee62834f90bb864c8adf232c3
rober97 <roberastorgab@gmail.com>
2023-01-12 10:33:09 -0300

    NO SE

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+var history = require('connect-history-api-fallback');
 app = express();
 //add this middleware
 app.use(history());    
