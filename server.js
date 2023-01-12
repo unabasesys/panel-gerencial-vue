01f39ab527f7391f8e75b6c6a2c7dbad2db78e18
rober97 <roberastorgab@gmail.com>
2023-01-12 09:42:19 -0300

    NO SE

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 app.listen(port);
